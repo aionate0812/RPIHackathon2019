57ca0d408a0aeeb4be1fb6627f5051b29699b2cc
Alexander <aionate0812@gmail.com>
2019-03-17 02:20:16 -0400

    completed for loop content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import ForLoop from './Containers/ForLoop/ForLoop'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-    
+        <ForLoop />
       </div>
     );
   }
